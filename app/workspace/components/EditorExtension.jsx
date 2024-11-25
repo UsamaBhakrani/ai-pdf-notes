f8ae259458dece932f952fc47316ecdbb839e5ff
Usama Bhakrani <usamabhakrani@gmail.com>
2024-11-25 18:24:03 +0500

    Tip tap editor extensions

--- app/workspace/components/EditorExtension.jsx
+++ app/workspace/components/EditorExtension.jsx
@@ -1,22 +1,80 @@
-import Bold from "@tiptap/extension-bold";
-import Document from "@tiptap/extension-document";
-import Paragraph from "@tiptap/extension-paragraph";
-import Text from "@tiptap/extension-text";
+import {
+  BoldIcon,
+  Heading1Icon,
+  Heading2Icon,
+  Heading3Icon,
+  ItalicIcon,
+  UnderlineIcon,
+  CodeIcon,
+  LinkIcon,
+  StrikethroughIcon,
+  BulletListIcon,
+} from "lucide-react";
 
 const EditorExtension = ({ editor }) => {
   return (
-    <div>
-      <div className="control-group">
-        <div className="button-group">
-          <button
-            onClick={() => editor.chain().focus().toggleBold().run()}
-            // className={editor.isActive("bold") ? "is-active" : ""}
-          >
-            Toggle bold
-          </button>
+    editor && (
+      <div className="p-5">
+        <div className="control-group">
+          <div className="flex gap-3 button-group">
+            <button
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 1 }).run()
+              }
+              className={
+                editor.isActive("heading", { level: 1 }) ? "text-blue-500" : ""
+              }
+            >
+              <Heading1Icon />
+            </button>
+            <button
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 2 }).run()
+              }
+              className={
+                editor.isActive("heading", { level: 2 }) ? "text-blue-500" : ""
+              }
+            >
+              <Heading2Icon />
+            </button>
+            <button
+              onClick={() =>
+                editor.chain().focus().toggleHeading({ level: 3 }).run()
+              }
+              className={
+                editor.isActive("heading", { level: 3 }) ? "text-blue-500" : ""
+              }
+            >
+              <Heading3Icon />
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleBold().run()}
+              className={editor?.isActive("bold") ? "text-blue-500" : ""}
+            >
+              <BoldIcon />
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleItalic().run()}
+              className={editor?.isActive("italic") ? "text-blue-500" : ""}
+            >
+              <ItalicIcon />
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleUnderline().run()}
+              className={editor?.isActive("underline") ? "text-blue-500" : ""}
+            >
+              <UnderlineIcon />
+            </button>
+            <button
+              onClick={() => editor.chain().focus().toggleCode().run()}
+              className={editor?.isActive("code") ? "text-blue-500" : ""}
+            >
+              <CodeIcon />
+            </button>
+          </div>
         </div>
       </div>
-    </div>
+    )
   );
 };
 
--- app/workspace/components/TextEditor.jsx
+++ app/workspace/components/TextEditor.jsx
@@ -4,11 +4,28 @@ import { useEditor, EditorContent } from "@tiptap/react";
 import StarterKit from "@tiptap/starter-kit";
 import Placeholder from "@tiptap/extension-placeholder";
 import EditorExtension from "./EditorExtension";
+import Bold from "@tiptap/extension-bold";
+import Document from "@tiptap/extension-document";
+import Paragraph from "@tiptap/extension-paragraph";
+import Text from "@tiptap/extension-text";
+import Heading from "@tiptap/extension-heading";
+import Underline from "@tiptap/extension-underline";
 
 const TextEditor = () => {
   const editor = useEditor({
     extensions: [
+      Document,
+      Paragraph,
+      Text,
       StarterKit,
+      Underline.configure({
+        HTMLAttributes: {
+          class: "my-custom-class",
+        },
+      }),
+      Heading.configure({
+        levels: [1, 2, 3],
+      }),
       Placeholder.configure({
         placeholder: "Write somethingasdasd…",
       }),
